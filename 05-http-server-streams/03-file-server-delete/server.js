f4ad82a2ee155451d6340a3806806a073a088b2c
Mikhail Vedernikov <mikhail.vedernikov@rt.ru>
2020-08-13 22:28:38 +0300

    add solution for 05-http-server-streams 03-file-server-delete

--- 05-http-server-streams/03-file-server-delete/server.js
+++ 05-http-server-streams/03-file-server-delete/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -11,6 +12,27 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'DELETE':
+      if (pathname.split('/').length > 1) {
+        res.statusCode = 400;
+        return res.end('Subdirectories are not supported');
+      }
+
+      fs.stat(filepath, (err) => {
+        if (err) {
+          res.statusCode = 404;
+          return res.end('File not found');
+        }
+
+        fs.unlink(filepath, (err) => {
+          if (err) {
+            res.statusCode = 500;
+            return res.end('Internal server error');
+          }
+
+          res.statusCode = 200;
+          res.end('File successfully removed');
+        });
+      });
 
       break;
 

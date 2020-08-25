1441d9e9088992a37fa0ef567dd9fa173e68a286
Mikhail Vedernikov <mikhail.vedernikov@rt.ru>
2020-08-25 11:30:37 +0300

    add solution for 07-mongodb-mongoose 02-rest-api

--- 07-mongodb-mongoose/02-rest-api/controllers/categories.js
+++ 07-mongodb-mongoose/02-rest-api/controllers/categories.js
@@ -1,3 +1,18 @@
+const Category = require('../models/Category');
+
+const mapSubcategoryToInternal = (subcategory) => ({
+  id: subcategory._id,
+  title: subcategory.title,
+});
+
+const mapCategoryToInternal = (category) => ({
+  id: category._id,
+  title: category.title,
+  subcategories: category.subcategories.map(mapSubcategoryToInternal),
+});
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+  const categories = await Category.find().populate('subcategories');
+
+  ctx.body = {categories: categories.map(mapCategoryToInternal)};
 };
